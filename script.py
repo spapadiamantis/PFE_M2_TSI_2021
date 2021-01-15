073667cda257e7297cc1fe96df34d093a4e09806
Sotiris Papadiamantis <sotirispapadiamantis@gmail.com>
2021-01-15 19:51:46 +0200

    Add displacement calculations

--- script.py
+++ script.py
@@ -41,7 +41,12 @@ sphFV2 = sio.load_mesh(mesh_file)
 transFV2=FV2
 transFV2.vertices = (FV2.vertices.dot(M2[0:3,0:3].T) + np.matlib.repmat((M2[0:3,3]-M1[0:3,3]).T , len(FV2.vertices),1)).dot(np.linalg.inv(M1[0:3,0:3]).T)
 
+# TO DO resample 2nd surface
 
 # Visualization of first surface
 visb_sc = splt.visbrain_plot(mesh=FV1, tex=texture1,caption='Surface 1 - ROI')
 visb_sc.preview()
+
+# Calculate displacement between two surfaces
+# transFV2 must be replaced with resampled surface
+displacement= np.sqrt(np.array((transFV2.vertices-FV1.vertices[0:len(transFV2.vertices)])**2).sum(axis=1))
