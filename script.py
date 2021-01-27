f8be95cbf1814da0e1834bacfbacaa95af556233
Sotiris Papadiamantis <sotirispapadiamantis@gmail.com>
2021-01-27 18:06:38 +0200

    Add mesh spectrum calculations

--- script.py
+++ script.py
@@ -111,5 +111,21 @@ visb_sc.preview()
 # Segment mesh based on texture
 sub_meshes, sub_tex, sub_corresp = stop.cut_mesh(FV1,texture1)
 
-# Calculate laplacian eigenvectors
-V = sdg.mesh_laplacian_eigenvectors(sub_meshes[0])
+# Calculate laplacian eigenvectors for graphs
+
+# Get laplacian matrix (performed 2x)
+_,B1 = sdg.compute_mesh_laplacian(transFV2, lap_type='fem')
+# Calculate eigenvectors
+V1 = sdg.mesh_laplacian_eigenvectors(transFV2)
+
+# Get laplacian matrix (performed 2x)
+_,B2 = sdg.compute_mesh_laplacian(FV1, lap_type='fem')
+# Calculate eigenvectors
+V2 = sdg.mesh_laplacian_eigenvectors(FV1)
+
+# Get spectrum for meshes
+spectrum1=np.transpose(V1)*B1*np.transpose(transFV2.vertices)
+spectrum2=np.transpose(V2)*B2*np.transpose(FV1.vertices)
+
+# Get spectrum of displacement
+spectrum_displacement=np.transpose(V1)*B1*displacement
